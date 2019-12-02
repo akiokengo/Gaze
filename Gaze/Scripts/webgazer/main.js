d2397a15e97a1b833f92fa191e5a365906c665ae
健吾 秋月 <zealoscry@outlook.jp>
2019-12-02 22:17:26 +0900

    テスト２

--- Gaze/Scripts/webgazer/main.js
+++ Gaze/Scripts/webgazer/main.js
@@ -19,27 +19,27 @@ window.onload = function() {
     }
 
     //start the webgazer tracker
-    //webgazer.setRegression('ridge') /* currently must set regression and tracker */
-    //    .setTracker('clmtrackr')
-    //    .setGazeListener(function (data, clock) {
-    //           console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-    //           console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
-    //    })
-    //    .beginAsync()
-    //    .always(function (w) {
-    //        w.showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
-    //        setTimeout(checkIfReady, 100);
-    //    });
-
-    //start the webgazer tracker
     webgazer.setRegression('ridge') /* currently must set regression and tracker */
         .setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        .setGazeListener(function (data, clock) {
+               console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+               console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
-        .begin()
-        .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+        .beginAsync()
+        .always(function (w) {
+            w.showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
+            setTimeout(checkIfReady, 100);
+        });
+
+    ////start the webgazer tracker
+    //webgazer.setRegression('ridge') /* currently must set regression and tracker */
+    //    .setTracker('clmtrackr')
+    //    .setGazeListener(function(data, clock) {
+    //      //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+    //      //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+    //    })
+    //    .begin()
+    //    .showPredictionPoints(true); /* shows a square every 100 milliseconds where current prediction is */
 
 
     //Set up the webgazer video feedback.
