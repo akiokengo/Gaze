527b3ed07ae1ea8a6996753d72012f3cd2cf9975
バター風味 <kotanameko0301@icloud.com>
2019-12-08 16:23:23 +0900

    googleもどき画面をTypeScriptでボタン押下のイベント処理等をするgoogle_Ctrl.tsファイルを作成。googleもどきの検索ボタンでアラーム動作確認完了

---
+++ Gaze/GoogleCtrl.ts
@@ -0,0 +1,6 @@
+﻿function searchButton_click() {
+	alert('こんにちわ');
+}
+
+var btn1b = document.getElementById('button1b');
+btn1b.onclick =  searchButton_click;
---
+++ Gaze/Scripts/google_Ctlr.ts
@@ -0,0 +1,6 @@
+﻿function searchButton_click() {
+	alert('検索ボタンが押されました');
+}
+
+var btn1b = document.getElementById('searchButton');
+btn1b.onclick =searchButton_click;
