84ddeeff9ebbb9eeff5b98bdd066b1632696b7d5
Shinichi Ueno <uen.agile@gmail.com>
2019-12-13 16:20:39 +0900

    ようつべの自動再生対応

--- Gaze/Scripts/play.js
+++ Gaze/Scripts/play.js
@@ -7,9 +7,29 @@ var Gaze;
             let iframe = document.getElementById("ytplayer");
             if (iframe) {
                 iframe.src = `https://www.youtube.com/embed/${v}?autoplay=1`;
-                iframe.width = `${window.screen.width * 0.8}px`;
-                iframe.height = `${window.screen.height * 0.8}px`;
+                iframe.width = "100%";
+                let buttons = document.getElementById("_buttnos");
+                if (buttons) {
+                    iframe.height = `${window.screen.height - ($(buttons).height() + 150)}px`;
+                }
+                //iframe.height = `${window.screen.height - 300}px`;
             }
+            let pause = document.getElementById("_pause");
+            if (pause) {
+                pause.onclick = e => {
+                    this.SendCommand("pauseVideo");
+                };
+            }
+            let play = document.getElementById("_play");
+            if (play) {
+                play.onclick = e => {
+                    this.SendCommand("playVideo");
+                };
+            }
+        }
+        SendCommand(command) {
+            let iframe = document.getElementById("ytplayer");
+            iframe.contentWindow.postMessage('{"event":"command","func":"' + command + '","args":""}', '*');
         }
     }
     Gaze.PlayView = PlayView;
--- Gaze/Scripts/play.ts
+++ Gaze/Scripts/play.ts
@@ -7,9 +7,36 @@
             let iframe = document.getElementById("ytplayer") as HTMLIFrameElement;
             if (iframe) {
                 iframe.src = `https://www.youtube.com/embed/${v}?autoplay=1`;
-                iframe.width = `${window.screen.width * 0.8}px`;
-                iframe.height = `${window.screen.height * 0.8}px`;
+                iframe.width = "100%";
+
+                let buttons = document.getElementById("_buttnos") as HTMLIFrameElement;
+                if (buttons) {
+                    iframe.height = `${window.screen.height - ($(buttons).height() + 150)}px`;
+                }
+
+                //iframe.height = `${window.screen.height - 300}px`;
+
             }
+
+            let pause = document.getElementById("_pause") as HTMLButtonElement;
+            if (pause) {
+                pause.onclick = e => {
+                    this.SendCommand("pauseVideo");
+                };
+            }
+
+            let play = document.getElementById("_play") as HTMLButtonElement;
+            if (play) {
+                play.onclick = e => {
+                    this.SendCommand("playVideo");
+                };
+            }
+
+        }
+
+        protected SendCommand(command: string) {
+            let iframe = document.getElementById("ytplayer") as HTMLIFrameElement;
+            iframe.contentWindow.postMessage('{"event":"command","func":"' + command + '","args":""}', '*');
         }
     }
 
